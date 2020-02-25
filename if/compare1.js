ce01c6b9c090f65461ef6c7dbd218b3cf2559465
황상진 <sangjin5579@naver.com>
2020-02-26 00:04:13 +0900

    compare algorithm update

---
+++ if/compare1.js
@@ -0,0 +1,40 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let result = '';
+
+function validator(num1, num2) {
+    return (num1 >= -10000 
+         && num1 <=  10000 
+         && num2 >= -10000 
+         && num2 <=  10000
+         && !isNaN(num1)
+         && !isNaN(num2))
+         && num1
+         && num2
+         ? true : false;
+}
+
+rl.on('line', line => {
+    const input = [];
+    input[0] = parseInt(line.split(' ')[0]);
+    input[1] = parseInt(line.split(' ')[1]);
+    if(validator(input[0], input[1])){
+        if(input[0] > input[1]) {
+            result = '>';
+        }
+        else if(input[0] < input[1]) {
+            result = '<';
+        }
+        else if(input[0] == input[1]) {
+            result = '==';
+        }
+        rl.close();
+    } else console.log('공백을 간격으로 하는 -10,000이상 10,000이하의 값을 입력해주세요.');
+}).on('close', () => {
+    console.log(result);
+    process.exit();
+});
