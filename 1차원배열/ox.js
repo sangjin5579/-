01ac2fa4e32ac6a7a8729903707c54731d822f80
황상진 <sangjin5579@naver.com>
2020-03-06 04:01:41 +0900

    ox algorithm update

---
+++ 1차원배열/ox.js
@@ -0,0 +1,19 @@
+//풀이x
+const [n, ...cases] = require('fs').readFileSync(0, 'utf8').trim().split('\n');
+
+let arrs = [];
+let result = [];
+
+for(let i=0;i<n;i++){
+    arrs[i] = [...cases[i]];
+    result[i] = 0;
+
+    let leng = arrs[i].length;
+    for(let j=0;j<leng;j++){
+        if(arrs[i][j] === 'O'){
+            arrs[i][j] = (arrs[i][j-1]) ? arrs[i][j-1]+1 : 1;
+        }else arrs[i][j] = 0;
+        result[i] += arrs[i][j];
+    }
+    console.log(result[i]);
+}
