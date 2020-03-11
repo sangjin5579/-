337f13455329895dc7d8f60f5960f01584030dbb
황상진 <sangjin5579@naver.com>
2020-03-12 00:49:18 +0900

    update

--- 함수/한수.js
+++ 함수/한수.js
@@ -1,19 +1,40 @@
-const n = require('fs').readFileSync(0,'utf8');
+// const n = require('fs').readFileSync(0,'utf8');
+
+// let cnt = n;
+
+// for(let i=1;i<=n;i++){
+//     const arr = [...i.toString()];
+//     const diff = (arr.length>=2) ? +arr[1] - +arr[0] : 0;
+
+//     for(let j=0,leng=arr.length;j<leng;j++){
+//         if(j+1<leng && diff !==  +arr[j+1] - +arr[j]){
+//             cnt--;
+//             break
+//         }
+//     }
+// }
+
+// console.log(cnt);
+
+
+let n = require('fs').readFileSync(0,'utf8');
 
 let cnt = n;
 
-for(let i=1;i<=n;i++){
-    const arr = [...i.toString()];
-    if(arr.length >=2){
-        const diff = +arr[1] - +arr[0] ;
-
-        for(let j=0,leng=arr.length;j<leng;j++){
-            if(diff !==  +arr[j+1] - +arr[j] && j+1<leng){
-                cnt--;
-                break
-            }
+let hanValidator = num => {
+    const arr = [...num.toString()];
+    const diff = (arr.length>=2) ? +arr[1] - +arr[0] : 0;
+    
+    for(let i=0,leng=arr.length;i<leng;i++){
+        if(i+1<leng && diff !==  +arr[i+1] - +arr[i]){
+            return false
         }
     }
+    return true    
 }
 
-console.log(cnt)
+for(let i=1;i<=n;i++){
+    if(!hanValidator(i)) cnt--;
+}
+
+console.log(cnt);
