010aadd8382f87e96dd0912840e53cdbb9986ffd
황상진 <sangjin5579@naver.com>
2020-03-14 20:06:16 +0900

    단어공부 algorithm update

--- 문자열/단어공부.js
+++ 문자열/단어공부.js
@@ -1,28 +1,33 @@
-const test = [...require('fs').readFileSync(0,'utf8').trim()];
+// const test = [...require('fs').readFileSync(0,'utf8').trim()];
 
-let transStr = code => {
-    return (65<=code && code<=90) ? code : code-32;
-}
+// let transStr = code => {
+//     return (65<=code && code<=90) ? code : code-32;
+// }
 
-const result = test.reduce((obj, str) => {
-    console.log(str)
-    let code = transStr(+str.charCodeAt(0));
-    console.log(code)
-    if(obj[code]){
-        obj[code]++
-    }else{
-        obj[code] = 1;
-    }
+// const result = test.reduce((obj, str) => {
+//     let code = transStr(+str.charCodeAt(0));
+//     if(obj[code]){
+//         obj[code]++
+//     }else{
+//         obj[code] = 1;
+//     }
     
-    if(obj.max.cnt<obj[code]){
-        obj.max.name = code;
-        obj.max.cnt = obj[code];
-        obj.max.overlap = false;
-    }else if(obj.max.cnt==obj[code]) obj.max.overlap = true;
-    console.log(obj)
-    return obj;
-}, {max:{name:0, cnt:0, overlap: false}});
+//     if(obj.max.cnt<obj[code]){
+//         obj.max.name = code;
+//         obj.max.cnt = obj[code];
+//         obj.max.overlap = false;
+//     }else if(obj.max.cnt==obj[code]) obj.max.overlap = true;
+//     return obj;
+// }, {max:{name:0, cnt:0, overlap: false}});
  
-if(result.max.overlap){
-    console.log('?');
-}else console.log(String.fromCharCode(result.max.name));
+// if(result.max.overlap){
+//     console.log('?');
+// }else console.log(String.fromCharCode(result.max.name));
+
+
+//1152
+const input = require('fs').readFileSync(0,'utf8').trim().split(' ')
+if(input[0] == '') {
+    console.log(0)
+}else console.log(input.length);  
+
