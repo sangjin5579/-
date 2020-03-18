31750baf62fa1ad94966c2cd369682348c4770b5
황상진 <sangjin5579@naver.com>
2020-03-18 22:01:52 +0900

    그룹단어체크 algorithm update

---
+++ 문자열/그룹단어체크.js
@@ -0,0 +1,29 @@
+//풀이 안함
+const [n, ...cases] = require('fs').readFileSync(0,'utf8').split('\n');
+
+let cnt = 0;
+
+let checkStr = (obj, str, i, arr) => {
+    if(obj[str] == undefined){
+        obj[str] = true
+        obj.result = true
+    }else{
+        if(arr[i-1] === str){
+            obj[str] = true
+        }else{
+            obj[str] = false;
+            obj.result = false;
+        }
+    }
+    return obj;
+}  
+
+for(let i=0;i<n;i++){
+    if(cases[i].trim() == '') {
+
+    }else{
+        cnt += ([...cases[i]].reduce(checkStr,{}).result) ? 1 : 0
+    }
+}
+
+console.log(cnt);
