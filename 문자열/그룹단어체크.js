35aa1ba5bf7d95c6997195c3b5488b18043ae228
황상진 <sangjin5579@naver.com>
2020-03-21 01:47:14 +0900

    그룹단어체크 algorithm update

--- 문자열/그룹단어체크.js
+++ 문자열/그룹단어체크.js
@@ -3,12 +3,12 @@ const [n, ...cases] = require('fs').readFileSync(0,'utf8').split('\n');
 
 let cnt = 0;
 
-let checkStr = (obj, str, i, arr) => {
-    if(obj[str] == undefined){
-        obj[str] = true
-        obj.result = true
+let checkStr = (obj, str, index, arr) => {
+    if(index === 0) obj.result = true;
+    if(obj[str] === undefined){
+        obj[str] = true;
     }else{
-        if(arr[i-1] === str){
+        if(arr[index-1] === str){
             obj[str] = true
         }else{
             obj[str] = false;
@@ -19,11 +19,7 @@ let checkStr = (obj, str, i, arr) => {
 }  
 
 for(let i=0;i<n;i++){
-    if(cases[i].trim() == '') {
-
-    }else{
-        cnt += ([...cases[i]].reduce(checkStr,{}).result) ? 1 : 0
-    }
+    cnt += ([...cases[i]].reduce(checkStr,{}).result) ? 1 : 0
 }
 
-console.log(cnt);      
+console.log(cnt);  
