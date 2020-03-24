a0b0d99707042091b8a18d8ed01f8650dd3f073f
황상진 <sangjin5579@naver.com>
2020-03-25 01:45:29 +0900

    벌집 algorithm update

---
+++ 수학1/벌집.js
@@ -0,0 +1,16 @@
+const N = +require('fs').readFileSync(0,'utf8');
+
+let cycle = 1;
+let endPoint = 1;
+
+if(N === 1) console.log(cycle);
+
+while(N !== 1){ 	
+	cycle++
+    endPoint += 6*(cycle-1);
+	
+    if(N<=endPoint){
+        console.log(cycle);
+        break
+    }
+}
