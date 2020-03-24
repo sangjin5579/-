ce21b2c9901e70def5b0a901eea0e5ff2364c6e6
황상진 <sangjin5579@naver.com>
2020-03-24 23:28:49 +0900

    설탕배달 algorithm update

--- 수학1/설탕배달.js
+++ 수학1/설탕배달.js
@@ -1,6 +1,14 @@
-const n = require('fs').readFileSync(0,'utf8');
-console.log((+n)%5)
-console.log((+n)%5%3)
-if((+n)%5%3 === 0){
-    console.log( Math.floor((+n)/5) + (+n)%5/3);    
-}else console.log(-1); 
+const n = +require('fs').readFileSync(0,'utf8');
+
+let a = 0;
+while(true){
+    if(n-3*a<0){
+        console.log(-1)
+        break;
+    }else if((n-3*a)%5 === 0){
+        console.log(a+(n-3*a)/5);
+        break;
+    }
+
+    a++
+}
