1e57c9b493b45f795772d06dc282b7012aa33aee
황상진 <sangjin5579@naver.com>
2020-04-11 21:27:56 +0900

    update

--- 수학1/설탕배달.js
+++ 수학1/설탕배달.js
@@ -3,7 +3,7 @@ const n = +require('fs').readFileSync(0,'utf8');
 let a = 0;
 while(true){
     if(n-3*a<0){
-        console.log(-1)
+        console.log(-1)  
         break;
     }else if((n-3*a)%5 === 0){
         console.log(a+(n-3*a)/5);
