a3ab2b543ae17f23654d56bdc1d8d0c6f7942fc5
황상진 <sangjin5579@naver.com>
2020-04-12 23:31:19 +0900

    test update

--- if/test.js
+++ if/test.js
@@ -1,45 +1,44 @@
-const tickets = [["ICN", "SFO"], ["ICN", "ATL"], ["SFO", "ATL"], ["ATL", "ICN"], ["ATL", "SFO"]];
+// const lost = [2,3,4];
+// const n =7;
+// const reserve = [1,2,3,6];
 
-function solution(tickets) {
-    var answer = [];
-    let start = 'ICN';
-    
-    answer.push(start);
-    
-    while(tickets.length !== 0){
-        let exist = false;
-        let ticketIndex = 0;
-        for(let i=0,leng=tickets.length;i<leng;i++){
-            if(tickets[i][0] === start){
-                if(!exist){
-                    answer.push(tickets[i][1]);
-                    ticketIndex = i;
-                    exist = true;
-                }else{
-                    if(answer[answer.length-1].charCodeAt(0) > tickets[i][1].charCodeAt(0)){
-                        answer.pop();
-                        answer.push(tickets[i][1]);
-                        ticketIndex = i;
-                    }else if(answer[answer.length-1].charAt(0) === tickets[i][1].charAt(0)){
-                        if(answer[answer.length-1].charCodeAt(1) > tickets[i][1].charCodeAt(1)){
-                            answer.pop();
-                            answer.push(tickets[i][1]);
-                            ticketIndex = i;
-                        }else if(answer[answer.length-1].charAt(1) === tickets[i][1].charAt(1)){
-                            if(answer[answer.length-1].charCodeAt(2) > tickets[i][1].charCodeAt(2)){
-                                answer.pop();
-                                answer.push(tickets[i][1]);
-                                ticketIndex = i;
-                            }
-                        }
-                    }
-                }
-            }
+// function solution(n, lost, reserve) {
+//     var answer = n;
+//     for(let i=0,leng=lost.length;i<leng;i++){
+//         console.log(lost)
+//         console.log(reserve)
+//         if(reserve[reserve.indexOf(lost[i])]){
+//             reserve.splice(reserve.indexOf(lost[i]), 1);
+//         }else if(reserve[reserve.indexOf(lost[i]-1)]){
+//             reserve.splice(reserve.indexOf(lost[i]-1), 1);
+//         }else if(reserve[reserve.indexOf(lost[i]+1)]){
+//             reserve.splice(reserve.indexOf(lost[i]+1), 1);
+//         }else --answer;
+//     }
+//     return answer;
+// }
+
+// console.log(solution(n,lost,reserve));
+
+
+function solution(name) {
+    var answer = 0;
+    for(let i=0,leng=name.length;i<leng;i++){
+        let code = name[i].charCodeAt(0);
+        let move = code - 65;
+        if(move > 12){
+            console.log(26-move)  
+            answer += 26-move;
+            
+        }else{
+            console.log(move)
+            answer += move;
         }
-        start = answer[answer.length-1];
-        tickets.splice(ticketIndex,1);
+
+        if(i !== leng-1) answer++;
+
     }
     return answer;
 }
 
-console.log(solution(tickets));
+console.log(solution('JAZ'));
