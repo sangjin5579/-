67fa00de53448cbb3817bcbdc6f9ec899c74f463
황상진 <sangjin5579@naver.com>
2020-04-14 23:34:57 +0900

    update

--- if/test.js
+++ if/test.js
@@ -23,22 +23,73 @@
 
 function solution(name) {
     var answer = 0;
+    let arr = [];  
+    let direct = "right";
+    
     for(let i=0,leng=name.length;i<leng;i++){
-        let code = name[i].charCodeAt(0);
-        let move = code - 65;
-        if(move > 12){
-            console.log(26-move)  
-            answer += 26-move;
-            
-        }else{
-            console.log(move)
-            answer += move;
+        if(name[i] === 'A'){
+            arr[i] = 1;
+        }else arr[i] = 0;
+    }
+    
+    console.log(arr);
+
+    let l = 0;
+    while(l<name.length){
+        l++
+        if(arr[name.length-l] === 0 && arr[l] === 0){
+            console.log(0)
+            continue;
+        }else if(arr[l] === 0){
+            console.log(1)
+            break;
+        }else if(arr[name.length-l] === 0){
+            direct = 'left';
+            console.log(2)
+            break;
         }
+    }
 
-        if(i !== leng-1) answer++;
+    console.log(direct);
 
+    func(direct)
+    function func(direct){
+        let code = name[0].charCodeAt(0);
+        let move = code - 65;
+
+        if(move > 12){
+            answer += 26-move;
+        }else{
+            answer += move;
+        }   
+
+        if(direct === "right"){
+            for(let i=1,leng=name.length;i<leng;i++){
+                if(i !== leng-1) answer++;
+                code = name[i].charCodeAt(0);
+                move = code - 65;
+
+                if(move > 12){
+                    answer += 26-move;
+                }else{
+                    answer += move;
+                }   
+            }
+        }else{
+            for(let i=name.length-1;i>0;--i){
+                if(i !== 1) answer++;
+                code = name[i].charCodeAt(0);
+                move = code - 65;
+
+                if(move > 12){
+                    answer += 26-move;
+                }else{
+                    answer += move;
+                }
+            }
+        }
     }
     return answer;
 }
 
-console.log(solution('JAZ'));
+console.log(solution('JEROEN'));
