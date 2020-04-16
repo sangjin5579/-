9fefba9209301b29f3d54dd621a91877607dc5c7
황상진 <sangjin5579@naver.com>
2020-04-16 23:17:21 +0900

    update

--- if/test.js
+++ if/test.js
@@ -21,75 +21,129 @@
 // console.log(solution(n,lost,reserve));
 
 
-function solution(name) {  
+// function solution(name) {  
+//     var answer = 0;
+//     let arr = [];  
+//     let direct = "right";  
+    
+//     for(let i=0,leng=name.length;i<leng;i++){
+//         if(name[i] === 'A'){
+//             arr[i] = 1;
+//         }else arr[i] = 0;
+//     }
+    
+//     console.log(arr);
+
+//     let l = 0;
+//     while(l<name.length){
+//         l++
+//         if(arr[name.length-l] === 0 && arr[l] === 0){
+//             console.log(0)
+//             continue;
+//         }else if(arr[l] === 0){
+//             console.log(1)
+//             break;
+//         }else if(arr[name.length-l] === 0){
+//             direct = 'left';
+//             console.log(2)
+//             break;
+//         }
+//     }
+
+//     console.log(direct);
+
+//     func(direct)
+//     function func(direct){
+//         let code = name[0].charCodeAt(0);
+//         let move = code - 65;
+
+//         if(move > 12){
+//             answer += 26-move;
+//         }else{
+//             answer += move;
+//         }   
+
+//         if(direct === "right"){
+//             for(let i=1,leng=name.length;i<leng;i++){
+//                 if(i !== leng-1) answer++;
+//                 code = name[i].charCodeAt(0);
+//                 move = code - 65;
+
+//                 if(move > 12){
+//                     answer += 26-move;
+//                 }else{
+//                     answer += move;
+//                 }   
+//             }
+//         }else{
+//             for(let i=name.length-1;i>0;--i){
+//                 if(i !== 1) answer++;
+//                 code = name[i].charCodeAt(0);
+//                 move = code - 65;
+
+//                 if(move > 12){
+//                     answer += 26-move;
+//                 }else{
+//                     answer += move;
+//                 }
+//             }
+//         }
+//     }
+//     return answer;
+// }
+
+// console.log(solution('JEROEN'));
+
+
+
+// function solution(number, k) {
+//     var answer = '';
+//     let arr = [...number];
+//     let test = [];
+//     arr = arr.sort(function(a,b){
+//         return b - a;
+//     })
+//     for(let i=0;i<k;i++){
+//         test.push(arr.pop());
+//     }
+//     for(let i=0;i<test.length;i++){
+//         arr.splice
+//         arr.splice()
+//     }
+//     answer = arr.join('');
+//     return answer;
+// }
+
+
+function solution(people, limit) {
     var answer = 0;
-    let arr = [];  
-    let direct = "right";  
-    
-    for(let i=0,leng=name.length;i<leng;i++){
-        if(name[i] === 'A'){
-            arr[i] = 1;
-        }else arr[i] = 0;
-    }
-    
-    console.log(arr);
+    let cnt = 0;
 
-    let l = 0;
-    while(l<name.length){
-        l++
-        if(arr[name.length-l] === 0 && arr[l] === 0){
-            console.log(0)
-            continue;
-        }else if(arr[l] === 0){
-            console.log(1)
-            break;
-        }else if(arr[name.length-l] === 0){
-            direct = 'left';
-            console.log(2)
-            break;
-        }
-    }
+    people.sort(function(a,b){
+        return b-a
+    })
 
-    console.log(direct);
+    while(true){
+        if(people.length === 0) break;
+        cnt++;
 
-    func(direct)
-    function func(direct){
-        let code = name[0].charCodeAt(0);
-        let move = code - 65;
+        let max = people[0]
+        people.splice(0,1);
+        console.log(max);
+        console.log(people);
 
-        if(move > 12){
-            answer += 26-move;
-        }else{
-            answer += move;
-        }   
-
-        if(direct === "right"){
-            for(let i=1,leng=name.length;i<leng;i++){
-                if(i !== leng-1) answer++;
-                code = name[i].charCodeAt(0);
-                move = code - 65;
-
-                if(move > 12){
-                    answer += 26-move;
-                }else{
-                    answer += move;
-                }   
-            }
-        }else{
-            for(let i=name.length-1;i>0;--i){
-                if(i !== 1) answer++;
-                code = name[i].charCodeAt(0);
-                move = code - 65;
-
-                if(move > 12){
-                    answer += 26-move;
-                }else{
-                    answer += move;
-                }
+        for(let j=0;j<people.length;j++){
+            if(max + people[j] <= limit){
+                people.splice(people.indexOf(people[j]),1);
+                console.log('결과',people)
+                continue;
             }
         }
     }
+    
+    answer = cnt;
+    
     return answer;
 }
 
-console.log(solution('JEROEN'));
+console.log(solution([70, 50, 50, 50],100))
