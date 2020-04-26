2f8f3653f2b0e3ec2eb0af87845f7e5e78de4219
황상진 <sangjin5579@naver.com>
2020-04-26 23:13:47 +0900

    update

--- 탐욕법/단속카메라.js
+++ 탐욕법/단속카메라.js
@@ -2,12 +2,12 @@ let sample = [[-20,15], [-14,-5], [-18,-13], [-5,-3]];
 
 
 function solution(routes) {
-    var answer = 0;
-    let fast = [];
+    var camera = 0;
+    let fast;
     let sort = [];
-    while(sample.length !== 0){
+    while(routes.length !== 0){
         for(let i=0;i<routes.length;i++){
-            if(sort.length === 0){
+            if(!fast){
                 fast = routes[i];
             }else{
                 if(fast[0] > routes[i][0]){
@@ -18,12 +18,31 @@ function solution(routes) {
             }
         }
         sort.push(fast);
-        sample.splice(sample.indexOf(fast),1);
-        fast = [];
+        routes.splice(routes.indexOf(fast),1);
+        fast = null;
     }
 
-
-    return answer;
+    let range;  
+    for(let i=0;i<sort.length;i++){
+        if(!range){
+            range = sort[i];
+            camera++;
+        }else{
+            //범위안에 들어온다면
+            if(sort[i][0] < range[1]){
+                //그리고, 뒷번호까지 범위안에 들어온다면,
+                if(sort[i][1] < range[1]){
+                    range[1] = sort[i][1];
+                }
+            }else{
+                //범위안에 들어오지 않는다면
+                range = sort[i];
+                camera++;
+            }
+        }
+    }
+    
+    return camera;
 }
 
 console.log(solution(sample));
