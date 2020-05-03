10af2ef875486b6f84ac32a58d987f6528fdd67a
황상진 <sangjin5579@naver.com>
2020-05-04 00:37:55 +0900

    완전탐색/소수찾기 algorithm update

--- 완전탐색/소수찾기.js
+++ 완전탐색/소수찾기.js
@@ -1,8 +1,11 @@
-let testCase = "17"
+let testCase = "011"
 
 let sosuValidator = (num) => {
+    if(num === 0 || num === 1){
+        return false;
+    }
     for(let i=2;i<num;i++){
-        if(num % i === 0 || num === 1){
+        if(num % i === 0 && num !== 2){
             return false;
         }
     }
@@ -10,74 +13,31 @@ let sosuValidator = (num) => {
 }
 
 let makeNum = (numbers) => {
-    console.log(numbers)
     let result = [];
     function test(usedId,currentNum){
-        console.log(usedId)
-        console.log(currentNum)
-
-            for(let i=0;i<numbers.length;i++){
-                if(!usedId.includes(i)){
-                    usedId.push(i)
-                    result.push(+(currentNum+numbers[i]));
-                    test(usedId,currentNum+numbers[i]);
-                }
+        for(let i=0;i<numbers.length;i++){
+            if(!usedId.includes(i)){
+                usedId.push(i)
+                result.push(+(currentNum+numbers[i]));
+                test(usedId, currentNum+numbers[i]);
+                usedId.pop();
             }
+        }
     }
+    test([],0);
 
-    // for(let i=0;i<numbers.length;i++){
-    //     result.push(+numbers[i]);
-    //     for(let j=0;j<numbers.length;j++){
-    //         if(j !== i){
-    //             result.push(+(numbers[i]+number[j]))
-    //             for(let k=0;k<numbers.length;k++){
-    //                 if(k !== i && k !== j){
-    //                     result.push(+(numbers[i]+number[j]+number[k]))
-        
-    //                 }
-    //             }
-    //         }
-    //     }
-    // }
-    
-    //중복제거한 후 return
-    return [...new Set(test([],0))]
+    return [...new Set(result)]
 }
 
 function solution(numbers) {
     var answer = 0;
     let createdNum = makeNum([...numbers]);
-    console.log(createdNum)
+
     for(let i=0;i<createdNum.length;i++){
         if(sosuValidator(createdNum[i])){
             answer++;
         }
     }
-    // for(let i=0;i<numbers.length;i++){
-    //     let num = numbers[i];
-    //     if(sosuValidator(+num)){
-    //         if(sosu.indexOf(+num) === -1){
-    //             sosu.push(+num);
-    //         }
-    //     }
-    //     if(num.length < numbers.length){
-    //             for(let i=0;i<numbers.length;i++){
-    //                 let num = numbers[i];
-    //                 if(sosuValidator(+num)){
-    //                     if(sosu.indexOf(+num) === -1){
-    //                         sosu.push(+num);
-    //                     }
-    //                 }
-    //                 if(num.length < numbers.length){
-            
-    //                 }
-    //             }
-    //     }
-    // }
-    // numbers.sort((a,b)=>{
-    //     return a-b;
-    // })
-    
     
     return answer;
 }
